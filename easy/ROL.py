c670db3e9c84af0e25697c74a58680f83c603a82
MateuszW <u56172@student.wat.edu.pl>
2025-12-30 16:44:48 +0100

    Remove unused variable 'n' from ROL.py

--- easy/ROL.py
+++ easy/ROL.py
@@ -3,7 +3,6 @@ t = int(input())
 lista = []
 for i in range(t):
     dane = list(map(int, input().split()))
-    n = dane[0]
     liczby = dane[1:]
     liczby = liczby[1:] + liczby[:1]
     lista.append(liczby)
